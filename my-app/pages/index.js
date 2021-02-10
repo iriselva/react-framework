662b8a250978e5f6dac0f6d23574c039d3edc683
IrisElva <iriselva@gmail.com>
2021-02-10 15:07:59 +0000

    first commit

---
+++ my-app/pages/index.js
@@ -0,0 +1,71 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import { client } from '@utils/prismicPosts'
+import Post from '@components/Post'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+        </div>
+
+        <div className="posts">
+          {posts !== undefined &&
+            posts.map((p) => {
+              let title = p.title[0].text
+              let key = `${p.date}+${title}`
+              return <Post key={key} date={p.date} image={p.image} title={title} />
+            })}
+        </div>
+
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  // query() is empty on purpose!
+  // https://prismic.io/docs/rest-api/query-the-api/query-all-documents
+  const res = await client.query('')
+
+  const posts = res.results.map((p) => {
+    return p.data
+  })
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
